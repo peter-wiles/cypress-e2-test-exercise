44806030d483fe87c3439e70e0f208a57440287d
Thunyiwe Mkhize <thunyiwe.mkhize@chillisoft.co.za>
2022-10-28 15:45:12 +0200

    Added test to check empty email validation

--- roadmap/cypress/e2e/subscribing-with-email-address.cy.ts
+++ roadmap/cypress/e2e/subscribing-with-email-address.cy.ts
@@ -1,16 +1,29 @@
 import { should } from "chai";
-import {endpoints} from "../../lib/constants"
+import { endpoints } from "../../lib/constants"
 
 describe("when subscribing with email address", () => {
-  it("should go to the homepage", () => {
-    cy.visit(endpoints.localhost);
-    cy.request(endpoints.clearApiEndpoint);
-    const emailInput = cy.get('[data-cy="feature-email-address"]');
-    emailInput.type("email@gmail.com");
-    cy.get('[data-cy="feature-email-submit"]').click();
+    it("should go to the homepage", () => {
+        cy.visit(endpoints.localhost);
+    });
 
-    emailInput.should("have.value","");
-    cy.get('.toast').children().should('have.text','You are now subscribed to feature updates!');
-  });
+    it("should succesfully subscribe", () => {
+        cy.request(endpoints.clearApiEndpoint);
+        const emailInput = cy.get('[data-cy="feature-email-address"]');
+        emailInput.type("email@gmail.com");
+        cy.get('[data-cy="feature-email-submit"]').click();
+
+        emailInput.should("have.value", "");
+        cy.get('.toast').children().should('have.text', 'You are now subscribed to feature updates!');
+    });
+
+    it("email address is required", () => {
+        cy.request(endpoints.clearApiEndpoint);
+        const emailInput = cy.get('[data-cy="feature-email-address"]').clear();
+        cy.get('[data-cy="feature-email-submit"]').click();
+
+        emailInput
+        .invoke("prop", "validationMessage")   
+        .should("eq", "Please fill out this field.");
+    });
 
 });
