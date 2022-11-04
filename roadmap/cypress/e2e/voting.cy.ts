7384f996a0936c57d857dcabf53641b92bfe0300
Thunyiwe Mkhize <thunyiwe.mkhize@chillisoft.co.za>
2022-11-04 15:34:06 +0200

    working on voting someone else's request

--- roadmap/cypress/e2e/voting.cy.ts
+++ roadmap/cypress/e2e/voting.cy.ts
@@ -14,6 +14,20 @@ const createRequestToAddAFeature = (id:any, featureTitle:any) => {
     }
 }
 
+const createRequestToVote = (id:any, featureTitle:any) => {
+    return {
+        method: "POST",
+        url: endpoints.voteApiEndpoint,
+        headers: {
+            "x-forwarded-for": "192.168.0.2",
+        },
+        body: {
+            id: id,
+            title: featureTitle,
+        }
+    }
+}
+
 describe("when voting on a request", () => {
   it("should vote on someones request", () => {
     const featureTitle = "cake";
@@ -29,5 +43,20 @@ describe("when voting on a request", () => {
         cy.get('[data-cy="feature-upvote-count"]').should("have.text", id + 1);
         cy.get('[data-cy="feature-upvote-button"]').should("have.class", "bg-green-100")
     });
+  });
+
+  it("should vote on someone elses request", () => {
+    
+    const input = "cake";
+    cy.request(endpoints.clearApiEndpoint);
+    const inputBox = cy.get('[data-cy="feature-input-box"]').type(input);
+    cy.get('[data-cy="feature-input-button"]').click();
+
+    cy.request(createRequestToVote("1aa12b33-a1af-4d38-8aab-0d85aa044d72", "a cool request")).then(() => {
+        cy.visit(endpoints.localhost);
+        cy.get('[data-cy="feature-upvote-count"]').should("have.text", 2);
+
+    });
+
   });
 });
