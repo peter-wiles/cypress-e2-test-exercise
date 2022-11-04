bd014be9ed20f4f41041233e8c9b7fd644669f13
Thunyiwe Mkhize <thunyiwe.mkhize@chillisoft.co.za>
2022-11-04 16:10:01 +0200

    Others voting on your request test case

--- roadmap/cypress/e2e/voting.cy.ts
+++ roadmap/cypress/e2e/voting.cy.ts
@@ -1,6 +1,6 @@
 import { endpoints } from "../../lib/constants";
 
-const createRequestToAddAFeature = (id:any, featureTitle:any) => {
+const createRequestToAddAFeature = (id: any, featureTitle: any) => {
     return {
         method: "POST",
         url: endpoints.createApiEndpoint,
@@ -14,12 +14,12 @@ const createRequestToAddAFeature = (id:any, featureTitle:any) => {
     }
 }
 
-const createRequestToVote = (id:any, featureTitle:any) => {
+const createRequestToVote = (id: any, featureTitle: any) => {
     return {
         method: "POST",
         url: endpoints.voteApiEndpoint,
         headers: {
-            "x-forwarded-for": "192.168.0.2",
+            "x-forwarded-for": "192.168.0.1",
         },
         body: {
             id: id,
@@ -29,34 +29,31 @@ const createRequestToVote = (id:any, featureTitle:any) => {
 }
 
 describe("when voting on a request", () => {
-  it("should vote on someones request", () => {
-    const featureTitle = "cake";
-    const id = 1;
-    cy.request(endpoints.clearApiEndpoint);
-    cy.request(createRequestToAddAFeature(id, featureTitle)).then(() => {
-        cy.visit(endpoints.localhost);
-        cy.get('[data-cy="feature-title"]').should("have.text", featureTitle);
-        cy.get('[data-cy="feature-upvote-count"]').should("have.text", id);
-        
-        cy.get('[data-cy="feature-upvote-button"]').click();
+    it("should vote on someones request", () => {
+        const featureTitle = "cake";
+        const id = 1;
+        cy.request(endpoints.clearApiEndpoint);
+        cy.request(createRequestToAddAFeature(id, featureTitle)).then(() => {
+            cy.visit(endpoints.localhost);
+            cy.get('[data-cy="feature-title"]').should("have.text", featureTitle);
+            cy.get('[data-cy="feature-upvote-count"]').should("have.text", id);
 
-        cy.get('[data-cy="feature-upvote-count"]').should("have.text", id + 1);
-        cy.get('[data-cy="feature-upvote-button"]').should("have.class", "bg-green-100")
-    });
-  });
+            cy.get('[data-cy="feature-upvote-button"]').click();
 
-  it("should vote on someone elses request", () => {
-    
-    const input = "cake";
-    cy.request(endpoints.clearApiEndpoint);
-    const inputBox = cy.get('[data-cy="feature-input-box"]').type(input);
-    cy.get('[data-cy="feature-input-button"]').click();
-
-    cy.request(createRequestToVote("1aa12b33-a1af-4d38-8aab-0d85aa044d72", "a cool request")).then(() => {
-        cy.visit(endpoints.localhost);
-        cy.get('[data-cy="feature-upvote-count"]').should("have.text", 2);
-
+            cy.get('[data-cy="feature-upvote-count"]').should("have.text", id + 1);
+            cy.get('[data-cy="feature-upvote-button"]').should("have.class", "bg-green-100")
+        });
     });
 
-  });
+    it("should vote on someone elses request", async () => {
+        const input = "cake";
+        cy.request(endpoints.clearApiEndpoint);
+        cy.get('[data-cy="feature-input-box"]').type(input);
+        cy.get('[data-cy="feature-input-button"]').click();
+
+        cy.request(createRequestToVote("edca09b0-7060-4f8d-a80a-315c68d53353", "cake")).then(() => {
+            cy.visit(endpoints.localhost);
+            cy.get('[data-cy="feature-upvote-count"]').should("have.text", 1);
+        });
+    });
 });
